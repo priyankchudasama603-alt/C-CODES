8ca97c36ba9c559faf3397426ada50b532a5ae48
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-02-03 18:20:57 +0530

    Add files via upload

---
+++ C/BasicsOfC_if_Else/Area_of_circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	float radius;
+	printf("Enter Radius:");
+	scanf("%f",&radius);
+	float area=3.14*radius*radius;
+	printf("The area of circle is:%f",area);
+	return 0;
+}
