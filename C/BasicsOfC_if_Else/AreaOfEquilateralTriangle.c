d7f23d53e0ee68a3dfb8407126eac5232b86e100
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-02-04 18:07:31 +0530

    Add files via upload

---
+++ C/BasicsOfC_if_Else/AreaOfEquilateralTriangle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    float length,area;
+	printf("Enter length of Triangle:");
+	scanf("%f",&length);
+	area=0.433*length*length; 
+    printf("The Area of Triangle is : %f",area);
+    return 0;  
+}
