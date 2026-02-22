ca32dc089f9b3988658900eb8f2c6fca3733f673
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-02-22 23:15:22 +0530

    Add files via upload

---
+++ C/BasicsOfC_if_Else/sumoftwonumber.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+	float a,b;
+	printf("Enter first number:");
+	scanf("%f",&a);
+	printf("Enter second number:");
+	scanf("%f",&b);
+	printf("Sum is:%.2f",a+b);
+	return 0;
+}
