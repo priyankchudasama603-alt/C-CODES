432087392ed4faab83bc42457aad0e7768566aae
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-02-24 15:14:53 +0530

    Add files via upload

---
+++ C/BasicsOfC_if_Else/SwapingM2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter Number a=");
+    scanf("%d",&a);
+    printf("Enter Number b=");
+    scanf("%d",&b);
+    a=a-b;
+    b=a+b;
+    a=b-a;
+    printf("After Swapping, a=%d\n",a);
+    printf("After Swapping, b=%d",b);
+    return 0;
+
+}
