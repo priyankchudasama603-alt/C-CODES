85092b23d63e8ae245cfe5a7da43c8d42b1d94fa
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-02-26 21:36:17 +0530

    Add files via upload

---
+++ C/LoopsInC/Apreverse.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int firstTerm=100,difference=-2;
+ 
+    int x;
+    
+    for (int  i = 0;x>0 ; i++)
+    {
+        x=  firstTerm  + i * difference;
+        printf("%d  ", x);
+    }
+    return 0;
+}
