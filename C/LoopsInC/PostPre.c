3e10d3c26b12f2f80961c86652e5348d1c724ed7
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-03-08 20:19:39 +0530

    Add files via upload

---
+++ C/LoopsInC/PostPre.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int x=5;
+    printf("%d\n",x);
+    printf("%d\n",x++);//x++ means use x then incriment,++x means incriment the use x
+    printf("%d\n",++x);
+}
