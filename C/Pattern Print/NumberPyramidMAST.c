60b8fe036d80eadbe277f4f30b6d5f56b3db76d9
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-04-09 00:17:37 +0530

    Add files via upload

---
+++ C/Pattern Print/NumberPyramidMAST.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter Number Of Row : ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        int a=i-1;
+        for(int j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=i;k++){
+            printf("%d",k);
+        }
+        for(int l=1;l<=i-1;l++){
+            printf("%d",a);
+            a--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
