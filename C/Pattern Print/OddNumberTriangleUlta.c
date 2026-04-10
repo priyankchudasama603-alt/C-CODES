98efba7d722c44afe1c34647fbd4696729a07d58
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-04-10 22:00:40 +0530

    Add files via upload

---
+++ C/Pattern Print/OddNumberTriangleUlta.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int n,sum=0;
+    printf("Enter Number of row : ");
+    scanf("%d",&n);
+    for(int i=1;i<=2*n-1;i+=2){
+        for(int j=1;j<=n+1-i;j+=2){
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
