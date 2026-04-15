fe7958fb123e277b92a832b5036178e42fc0d722
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-04-15 17:01:01 +0530

    Add files via upload

---
+++ C/Pattern Print/StarDiamondM1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter Number Of Row : ");
+    scanf("%d",&n);
+    int m=n/2+1;
+    int nsp=n/2;
+    int nst=1;
+    
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=nsp;j++){
+            printf(" ");
+        }
+        for(int k=1;k<=nst;k++){
+            printf("*");
+        }
+        printf("\n");
+        if(i<m){
+            nsp--;
+            nst+=2;
+        }
+        else{
+            nsp++;
+            nst-=2;
+        }
+    }
+    return 0;
+}
