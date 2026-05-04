399faceab03e13f0b6730f941b896b3124811bb1
priyankchudasama603-alt <priyankchudasama603@gmail.com>
2026-05-04 21:52:04 +0530

    Add files via upload

---
+++ C/Array/Sig27.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int myNumbers[]={22,4,5,1,11,40,16,80,45,18};
+    int sum=0;
+    for (int i =0;i<10;i++){
+        sum+=myNumbers[i];
+        
+        }
+    printf("%d",sum);
+
+    return 0;
+}
